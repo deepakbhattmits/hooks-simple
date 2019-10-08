9641ba3bed047e6c350dc85c877f4421a065f636
deepak bhatt <deepakbhattmits@gmail.com>
2019-10-08 14:04:21 +0530

    added new function

--- src/components/ResourceList.jsx
+++ src/components/ResourceList.jsx
@@ -4,14 +4,20 @@ import axios from "axios";
 
 const ResourceList = ({ resource }) => {
     const [resources, setResources] = useState([]);
-    const fetchResource = async (resource) => {
-        const resp = await axios.get(
-            `https://jsonplaceholder.typicode.com/${resource}`
-        );
-        setResources(resp.data);
-    };
+    // const fetchResource = async (resource) => {
+    //     const resp = await axios.get(
+    //         `https://jsonplaceholder.typicode.com/${resource}`
+    //     );
+    //     setResources(resp.data);
+    // };
     useEffect(() => {
-        fetchResource(resource);
+        // fetchResource(resource);
+        (async (resource) => {
+            const resp = await axios.get(
+                `https://jsonplaceholder.typicode.com/${resource}`
+            )
+            setResources(resp.data);
+        })(resource);
     }, [resource]);
     return <List resource={resources} />;
 };
